041b78c74b3ac938024086da8e494ff01c9d1084
Vincentia Indri Octaviani <19081010168@student.upnjatim.ac.id>
2022-10-12 09:42:20 +0700

    first commit

--- resources/views/index/admin.blade.php
+++ resources/views/index/admin.blade.php
@@ -8,57 +8,71 @@
 @endsection
 <!-- Content Row -->
 
-<div class="row">
-    <div class="col-md-12 grid-margin stretch-card">
-        <div class="card shadow">
+<div class="row ">
+    <div class="col-md-6 mb-4 stretch-card transparent">
+        <div class="card card-dark-blue">
             <div class="card-body">
-                <div class="row">
-                    <div class="col-12">
-                        <button type="button" class="btn btn-danger btn-lg btn-icon-text ">
-                            <i class="ti-upload btn-icon-prepend"></i>
-                            Upload
-                        </button>
-                    </div>
+                <p class="mb-4">Total Bookings</p>
+                <p class="fs-30 mb-2">61344</p>
+                <p>22.00% (30 days)</p>
+                <div class="pt-2">
+                    <button type="button" class="btn btn-danger btn-icon-text">
+                        <i class="ti-upload btn-icon-prepend"></i>
+                        Input Pengadaan
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 mb-4 stretch-card transparent">
+        <div class="card card-tale">
+            <div class="card-body">
+                <p class="mb-4">Total Bookings</p>
+                <p class="fs-30 mb-2">61344</p>
+                <p>22.00% (30 days)</p>
+                <div class="pt-2">
+                    <button type="button" class="btn btn-danger btn-icon-text">
+                        <i class="ti-upload btn-icon-prepend"></i>
+                        Input Anggaran
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
+
 <div class="row">
-    <div class="row">
-        <div class="col grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                    <div class="chartjs-size-monitor">
-                        <div class="chartjs-size-monitor-expand">
-                            <div class=""></div>
-                        </div>
-                        <div class="chartjs-size-monitor-shrink">
-                            <div class=""></div>
-                        </div>
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card shadow">
+            <div class="card-body">
+                <div class="chartjs-size-monitor">
+                    <div class="chartjs-size-monitor-expand">
+                        <div class=""></div>
                     </div>
-                    <div class="d-flex justify-content-between">
-                        <p class="card-title">Sales Report</p>
-                        <a href="#" class="text-info">View all</a>
+                    <div class="chartjs-size-monitor-shrink">
+                        <div class=""></div>
                     </div>
-                    <p class="font-weight-500">The total number of sessions within the date range. It is the period time a user is actively engaged with your website, page or app, etc</p>
-                    <div id="sales-legend" class="chartjs-legend mt-4 mb-2">
-                        <ul class="1-legend">
-                            <li>
-                                <span style="background-color: rgb(152, 189, 255);"></span>Offline Sales
-                            </li>
-                            <li>
-                                <span style="background-color: rgb(75, 73, 172);"></span>Online Sales
-                            </li>
-                        </ul>
-                    </div>
-                    {{-- tempalte1/js/dashboard.js --}}
-                    <canvas id="sales-chart" width="441" height="220" style="display: block; height: 147px; width: 294px;" class="chartjs-render-monitor"></canvas>
                 </div>
+                <div class="d-flex justify-content-between">
+                    <p class="card-title">Sales Report</p>
+                    <a href="#" class="text-info">View all</a>
+                </div>
+                <p class="font-weight-500">The total number of sessions within the date range. It is the period time a user is actively engaged with your website, page or app, etc</p>
+                <div id="sales-legend" class="chartjs-legend mt-4 mb-2">
+                    <ul class="1-legend">
+                        <li>
+                            <span style="background-color: rgb(152, 189, 255);"></span>Offline Sales
+                        </li>
+                        <li>
+                            <span style="background-color: rgb(75, 73, 172);"></span>Online Sales
+                        </li>
+                    </ul>
+                </div>
+                {{-- tempalte1/js/dashboard.js --}}
+                <canvas id="sales-chart" width="441" height="220" style="display: block; height: 147px; width: 294px;" class="chartjs-render-monitor"></canvas>
             </div>
         </div>
-
     </div>
 </div>
 
