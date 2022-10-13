e9d499c4b9599b0479bfa5678a15b7f72bb98476
SabrinaHeryantiSuryandari <19081010130@student.upnjatim.ac.id>
2022-10-13 14:34:19 +0700

    database

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -134,6 +134,7 @@
             {{-- Logout --}}
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
+                            <i class="ti-power-off text-primary"></i>
               {{ __('Logout') }}
             </a>
 
