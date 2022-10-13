4bb4d9b27e8d26bd0e4b389196e9361f71f4e54f
Vincentia Indri Octaviani <19081010168@student.upnjatim.ac.id>
2022-10-13 13:47:44 +0700

    first commit

--- resources/views/admin/pengadaan1.blade.php
+++ resources/views/admin/pengadaan1.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts/index')
 
 @section('content')
-    @section('judul')
-        {{'Input Data / Belum Tersertifikasi'}}
-    @endsection
-    @section('title')
-        {{'Input Data yang Belum Tersertifikasi'}}
-    @endsection
+@section('judul')
+{{'Input Data / Belum Tersertifikasi'}}
+@endsection
+@section('title')
+{{'Input Data yang Belum Tersertifikasi'}}
+@endsection
 
 <div class="row">
     {{-- <div class="col-lg-12 grid-margin stretch-card">
@@ -16,12 +16,12 @@
             @if ($message = Session::get('success'))
             <div class="alert alert-success">
                 <p>{{ $message }}</p>
-            </div>
-        @endif --}}
-            {{-- <p class="card-description">
+</div>
+@endif --}}
+<!-- {{-- <p class="card-description">
               Add class <code>.table-bordered</code>
-            </p> --}}
-            {{-- <div class="table-responsive pt-3">
+            </p> --}} -->
+<!-- {{-- <div class="table-responsive pt-3">
               <table class="table table-bordered">
                 <thead>
                     <tr>
@@ -45,72 +45,72 @@
             </div>
           </div>
         </div>
-    </div>  --}}
-    <div class="col-lg-12 grid-margin stretch-card">
-        <div class="card">
-    {{-- <div class="card shadow mb-3 "> --}}
+    </div>  --}} -->
+<div class="col-lg-12 grid-margin stretch-card">
+    <div class="card shadow">
+        <!-- {{-- <div class="card shadow mb-3 "> --}}
         {{-- <div class="card-header py-2 "style="background-color: #40B9CB;">
             <h6 class="m-0 font-weight-bold text-white text-center">Tabel Data Aset</h6>
-        </div> --}}
+        </div> --}} -->
         <div class="card-body">
-            <h4 class="card-title">Data Pengadaan <i class="fa-solid fa-less-than-equal "></i>  50 juta</h4>
+            <h4 class="card-title">Data Pengadaan <i class="fa-solid fa-less-than-equal "></i> 50 juta</h4>
             <div class="table-responsive">
                 @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
-                    </div>
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
                 @endif
 
                 <table class="table table-bordered" id="example" style="width:100%">
                     <thead>
-                    <tr>
-                        <th> NO </th>
-                        <th> ID </th>
-                        <th>No Sertifikat</th>
-                        <th>Nama Aset</th>
-                        <th>Alamat</th>
-                        <th>Status Aset</th>
-                        {{-- <th>Luas Aset</th> --}}
-                        {{-- <th>Kondisi Aset</th>
+                        <tr>
+                            <th> NO </th>
+                            <th> ID </th>
+                            <th>No Sertifikat</th>
+                            <th>Nama Aset</th>
+                            <th>Alamat</th>
+                            <th>Status Aset</th>
+                            {{-- <th>Luas Aset</th> --}}
+                            {{-- <th>Kondisi Aset</th>
                         <th>Kondisi Geografis</th>
                         <th>Asal Usul</th>
                         <th>Tahun Kepemilikan</th>
                         <th>Tahun Pembangunan</th>
                         <th>Tahun Rehab</th> --}}
-                        <th>Keterangan</th>
-                        <th width="280px">Action</th>
-                    </tr>
+                            <th>Keterangan</th>
+                            <th width="280px">Action</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {{-- @foreach ($asets as $aset) --}}
+                        <!-- {{-- @foreach ($asets as $aset) --}}
                         {{-- <tr>
                             <td>{{ ++$i }}</td>
-                            <td>{{ $aset->id}}</td>
-                            <td>{{ $aset->no_sertifikat }}</td>
-                            <td>{{ $aset->nama_aset }}</td>
-                            <td>{{ $aset->alamat_aset }}</td>
-                            <td>{{ $aset->status_aset }}</td>
+                        <td>{{ $aset->id}}</td>
+                        <td>{{ $aset->no_sertifikat }}</td>
+                        <td>{{ $aset->nama_aset }}</td>
+                        <td>{{ $aset->alamat_aset }}</td>
+                        <td>{{ $aset->status_aset }}</td>
 
-                            <td>{{ $aset->keterangan_aset }}</td>
-                            <td>
-                                
-                                <form action="{{ route('asets.destroy',$aset->id) }}" method="POST">
+                        <td>{{ $aset->keterangan_aset }}</td>
+                        <td>
 
-                                    <a class="btn btn-info" href="{{ route('asets.show',$aset->id) }}">Detail</a>
-                                    <a class="btn btn-primary" href="{{ route('asets.edit',$aset->id) }}">Edit</a>
-                                    @csrf
-                                    @method('DELETE')
+                            <form action="{{ route('asets.destroy',$aset->id) }}" method="POST">
 
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr> --}}
-                    {{-- @endforeach --}}
+                                <a class="btn btn-info" href="{{ route('asets.show',$aset->id) }}">Detail</a>
+                                <a class="btn btn-primary" href="{{ route('asets.edit',$aset->id) }}">Edit</a>
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                        </tr> --}} -->
+                        <!-- {{-- @endforeach --}} -->
                     </tbody>
                 </table>
             </div>
         </div>
-        </div>
     </div>
+</div>
 </div>
 @endsection
