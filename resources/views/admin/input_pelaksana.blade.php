93c50e2c3e283f849e3a146a2665e6db443a5269
Vincentia Indri Octaviani <19081010168@student.upnjatim.ac.id>
2022-10-18 08:45:43 +0700

    input pelaksana

--- resources/views/admin/input_pelaksana.blade.php
+++ resources/views/admin/input_pelaksana.blade.php
@@ -6,9 +6,10 @@
 {{-- @section('judul')
 {{'Input Data / Belum Tersertifikasi'}}
 @endsection -->
+
 <!-- @section('title')
 {{'Input Data yang Belum Tersertifikasi'}}
-@endsection --}}
+@endsection --}} -->
 
 <!-- Content Row -->
 
@@ -23,179 +24,48 @@
 </div>
 @endif
 
-{{-- <div class="col-12 grid-margin stretch-card">
-  <div class="card shadow">
-    <div class="card-body">
-      <h4 class="card-title">Masukan Data Pelaksana</h4>
-
-      
-    </div>
-  </div>
-</div> --}}
-
-<form action="{{ route('pelaksanas.store') }}" method="POST">
-    @csrf
-
-    <div class="col-12 grid-margin stretch-card">
-        <div class="card shadow">
-            <div class="card-body">
-                <h4 class="card-title">Data Masukan Pengadaan</h4>
+<div class="col-12 grid-margin stretch-card">
+    <div class="card shadow">
+        <div class="card-body">
+            <h4 class="card-title">Input Data Pelaksana</h4>
+            <form action="{{ route('pelaksanas.store') }}" method="POST">
+                @csrf
                 <table class="" style="margin:20px auto;" id="dataTable" width="100%" cellspacing="0">
-
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                <td>Nama CV Pelaksana</td>
-                                <td>
-                                    <input type="text" name="pt_pelaksana" value="{{ old('pt_pelaksana') }}" placeholder="CV Pelaksana">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('pt_pelaksana') }}</small>
-
-                                    {{-- <select name="pt_pelaksana" value="{{ old('pt_pelaksana') }}">
-                                    <option value="">----Pilih Status Aset----</option>
-                                    <option value="Tersertifikasi">Tersertifikasi</option>
-                                    <option value="Belum Tersertifikasi" selected>Belum Tersertifikasi</option>
-                                    </select>
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('pt_pelaksana') }}</small> --}}
-                                </td>
-
-                                <td>Telepon</td>
-                                <td>
-                                    <input type="text" name="tlp" value="{{ old('tlp') }}" placeholder="Telepon">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('tlp') }}</small>
-
-                                    {{-- <select name="asalusul_aset" value="{{ old('asalusul_aset') }}">
-                                    <option value="" selected>----Pilih Asal Usul Aset----</option>
-                                    <option value="Pembelian">Pembelian</option>
-                                    <option value="Hibah">Hibah</option>
-                                    </select>
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('asalusul_aset') }}</small> --}}
-                                </td>
-                            </tr>
+                            <label for="exampleInputName1">Nama CV Pelaksana</label>
+                            <input class="form-control" type="text" name="pt_pelaksana" value="{{ old('pt_pelaksana') }}" placeholder="CV Pelaksana">
+                            <small class="text-danger">{{ $errors->first('pt_pelaksana') }}</small>
                         </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                <td>Nama Pelaksana</td>
-                                <td>
-                                    <input type="text" name="nama_pelaksana" value="{{ old('nama_pelaksana') }}" placeholder="Nama Pelaksana">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('nama_pelaksana') }}</small>
-                                </td>
-
-                                <td>Jabatan Pelaksana</td>
-                                <td>
-                                    <input type="text" name="jabatan_pelaksana" value="{{ old('jabatan_pelaksana') }}" placeholder="Jabatan Pelaksana">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('jabatan_pelaksana') }}</small>
-                                </td>
-                                {{-- <td>
-                                        <select name="kondisi_aset" value="{{ old('kondisi_aset') }}">
-                                <option value="" selected>-----Pilih Kondisi Aset-----</option>
-                                <option value="Layak">Layak</option>
-                                <option value="Tidak Layak">Tidak Layak</option>
-                                <option value="Tidak Layak">Tidak ada bangunan</option>
-                                </select>
-                                <br>
-                                <small class="text-danger">{{ $errors->first('kondisi_aset') }}</small>
-                                </td> --}}
-                            </tr>
+                            <label for="exampleTextarea1">Alamat</label>
+                            <textarea class="form-control" name="alamat" value="{{ old('alamat') }}" rows="4" placeholder="Alamat"></textarea>
+                            <small class="text-danger">{{ $errors->first('alamat') }}</small>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                <td>Kode Rekening</td>
-                                <td>
-                                    <input id="text" name="kode_rekening" value="{{ old('kode_rekening') }}" placeholder="Kode Rekening">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('kode_rekening') }}</small>
-                                </td>
-
-                                <td>NPWP</td>
-                                <td>
-                                    <input id="text" name="npwp" value="{{ old('npwp') }}" placeholder="NPWP">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('npwp') }}</small>
-                                </td>
-                                {{-- <td>
-                                        <select name="kondisi_geografis" value="{{ old('kondisi_geografis') }}">
-                                <option value="" selected>--Pilih Kondisi Geografis--</option>
-                                <option value="Dataran Tinggi">Dataran Tinggi</option>
-                                <option value="Dataran Rendah">Dataran Rendah</option>
-                                <option value="Daerah Perairan">Daerah Perairan</option>
-                                </select>
-                                <br>
-                                <small class="text-danger">{{ $errors->first('kondisi_geografis') }}</small>
-                                </td> --}}
-                            </tr>
+                            <label for="exampleInputName1">Nama Pelaksana</label>
+                            <input class="form-control" type="text" name="nama_pelaksana" value="{{ old('nama_pelaksana') }}" placeholder="Nama Pelaksana">
+                            <small class="text-danger">{{ $errors->first('nama_pelaksana') }}</small>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                <td>Alamat</td>
-                                <td>
-                                    <input id="w3review" name="alamat" value="{{ old('alamat') }}" placeholder="Alamat">
-                                    <br>
-                                    <small class="text-danger">{{ $errors->first('alamat') }}</small>
-                                </td>
-
-                                {{-- <td>Tahun Rehabilitasi</td>
-                                    <td>
-                                        {{-- <input id="text" name="th_rehab" value="{{ old('th_rehab') }}"> --}}
-                                {{-- <input type="date" id="text" name="th_rehab" value="{{ old('th_rehab') }}" placeholder="Tahun Rehabilitasi">
-                                <br>
-                                <small class="text-danger">{{ $errors->first('th_rehab') }}</small>
-                                </td> --}}
-
-                            </tr>
+                            <label for="exampleInputName1">Jabatan Pelaksana</label>
+                            <input class="form-control" type="text" name="jabatan_pelaksana" value="{{ old('jabatan_pelaksana') }}" placeholder="Jabatan Pelaksana">
+                            <small class="text-danger">{{ $errors->first('jabatan_pelaksana') }}</small>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                {{-- <td>Luas Aset</td>
-                                    <td>
-                                        <input id="text" name="luas_aset" value="{{ old('luas_aset') }}" placeholder="Luas Aset">
-                                <br>
-                                <small class="text-danger">{{ $errors->first('luas_aset') }}</small>
-                                </td>
-
-                                <td>Tahun Kepemilikan</td>
-                                <td>
-                                    <input type="date" id="text" name="th_kepemilikan" value="{{ old('th_kepemilikan') }}"> --}}
-                                    {{-- <input id="text" name="th_kepemilikan" value="{{ old('th_kepemilikan') }}"> --}}
-                                    <br>
-                                    {{-- <small class="text-danger">{{ $errors->first('th_kepemilikan') }}</small>
-                                </td> --}}
-
-                            </tr>
+                            <label for="exampleInputName1">Kode Rekening</label>
+                            <input class="form-control" id="text" name="kode_rekening" value="{{ old('kode_rekening') }}" placeholder="Kode Rekening">
+                            <small class="text-danger">{{ $errors->first('kode_rekening') }}</small>
                         </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <tr>
-                                {{-- <td>Keterangan Aset</td>
-                                    <td>
-                                        <input id="text" name="keterangan_aset" value="{{ old('keterangan_aset') }}" placeholder="Keterangan">
-                                <br>
-                                <small class="text-danger">{{ $errors->first('keterangan_aset') }}</small>
-                                </td>
-
-                                <td>Tahun Pembangunan</td>
-                                <td>
-                                    <input type="date" id="text" name="th_pembangunan" value="{{ old('th_pembangunan') }}"> --}}
-                                    {{-- <input id="text" name="th_pembangunan" value="{{ old('th_pembangunan') }}"> --}}
-                                    {{-- <br>
-                                        <small class="text-danger">{{ $errors->first('th_pembangunan') }}</small>
-                                </td> --}}
-                            </tr>
+                            <label for="exampleInputName1">NPWP</label>
+                            <input class="form-control" id="text" name="npwp" value="{{ old('npwp') }}" placeholder="NPWP">
+                            <small class="text-danger">{{ $errors->first('npwp') }}</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputName1">Telephone</label>
+                            <input class="form-control" type="text" name="tlp" value="{{ old('tlp') }}" placeholder="Telepon">
+                            <small class="text-danger">{{ $errors->first('tlp') }}</small>
                         </div>
                     </div>
                 </table>
@@ -205,127 +75,15 @@
                             <td>
                                 <br>
                                 <button type="submit" class="btn btn-success text-center">Submit</button>
+                                <a href="/home" class="btn btn-light text-center">Kembali</a>
                             </td>
                         </tr>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
-</form>
+</div>
+
 
 @endsection
-
-{{-- <style>
-  li {
-    list-style-type: none;
-  }
-
-  .collapsible {
-    background-color: #ffffff;
-    color: black;
-    cursor: pointer;
-    padding: 18px;
-    width: 100%;
-    border: none;
-    text-align: left;
-    outline: none;
-    font-size: 15px;
-    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.5);
-  }
-
-  form .input:hover {
-    color: darkgrey;
-    background-color: white;
-    transition: 0.3s;
-  }
-
-  form .input {
-    font-family: "Roboto", sans-serif;
-    outline: 0;
-    color: grey;
-    background: #ffffff;
-    background-color: #e8e8e8;
-    width: 100%;
-    border: 0;
-    margin: 0 0 5px;
-    padding: 15px;
-    box-sizing: border-box;
-    border-radius: 3px;
-    transition: 0.3s;
-  }
-
-  form .inputselect:hover {
-    color: darkgrey;
-    background-color: white;
-    transition: 0.3s;
-  }
-
-  form .inputselect {
-    font-family: "Roboto", sans-serif;
-    outline: 0;
-    color: grey;
-    background: #ffffff;
-    background-color: #e8e8e8;
-    width: 100%;
-    border: 0;
-    margin: 0 0 5px;
-    padding: 15px;
-    box-sizing: border-box;
-    border-radius: 3px;
-    transition: 0.3s;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    content: "▼";
-    font-family: "Roboto", sans-serif;
-    outline: 0;
-    color: grey;
-    background: #ffffff;
-    width: 100%;
-    border: 0;
-    margin: 0 0 5px;
-    padding: 15px;
-    box-sizing: border-box;
-    border-radius: 3px;
-    transition: 0.3s;
-
-    background-image:
-      linear-gradient(45deg, transparent 50%, grey 50%),
-      linear-gradient(135deg, grey 50%, transparent 50%);
-
-    background-position:
-      calc(100% - 20px) calc(1em + 8px),
-      calc(100% - 15px) calc(1em + 8px),
-      100% 0;
-    background-size:
-      5px 5px,
-      5px 5px,
-      2.5em 2.5em;
-    background-repeat: no-repeat;
-  }
-
-  .content {
-    padding: 1rem;
-    /* padding: 0 18px; */
-    display: none;
-    overflow: hidden;
-    background-color: #f1f1f1;
-  }
-</style>
-
-<script>
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
-</script> --}}
